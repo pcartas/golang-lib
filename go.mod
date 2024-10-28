97743fabcbdc90d57f4afdab4c7efd8150ec9b9c
Paula <pcartas@arkho.io>
2024-10-28 12:35:35 -0300

    fix version

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/pcartas/golang-lib
 
-go 1.23.2
+go 1.22
 
 require github.com/gorilla/mux v1.8.1
--- router/router.go
+++ router/router.go
@@ -1,17 +1,18 @@
 package router
 
 import (
-    "net/http"
-    "github.com/gorilla/mux"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Route defines a single route, e.g. a human readable name, HTTP method and the pattern the function that will execute when the route is called.
 type Route struct {
-    Name        string
-    Method      string
-    Pattern     string
-    HandlerFunc http.HandlerFunc
-    Middlewares []mux.MiddlewareFunc
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+	Middlewares []mux.MiddlewareFunc
 }
 
 // Routes is a slice of Route structs.
@@ -19,26 +20,26 @@ type Routes []Route
 
 // NewRouter creates a new router with the provided routes and middlewares.
 func NewRouter(routes Routes, generalMiddlewares []mux.MiddlewareFunc) *mux.Router {
-    router := mux.NewRouter().StrictSlash(true)
-    
-    // Add general middlewares to the router
-    for _, middleware := range generalMiddlewares {
-        router.Use(middleware)
-    }
+	router := mux.NewRouter().StrictSlash(true)
 
-    // Register routes
-    for _, route := range routes {
-        r := router.
-            Methods(route.Method).
-            Path(route.Pattern).
-            Name(route.Name).
-            Handler(route.HandlerFunc)
-        
-        // Add specific middlewares to the route
-        for _, middleware := range route.Middlewares {
-            r.Handler(middleware(r.GetHandler()))
-        }
-    }
+	// Add general middlewares to the router
+	for _, middleware := range generalMiddlewares {
+		router.Use(middleware)
+	}
 
-    return router
+	// Register routes
+	for _, route := range routes {
+		r := router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.HandlerFunc)
+
+		// Add specific middlewares to the route
+		for _, middleware := range route.Middlewares {
+			r.Handler(middleware(r.GetHandler()))
+		}
+	}
+
+	return router
 }
